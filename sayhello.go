9363df6d8b23613be6730d0c3366fa5896e3494f
andiagusp <aguspurnomoandi@gmail.com>
2021-09-15 12:23:03 +0700

    second

--- sayhello.go
+++ sayhello.go
@@ -1,5 +1,11 @@
 package sayhello
 
+// SayHello is exported
 func SayHello(name string) string {
 	return "Hello " + name
+}
+
+// SayHay is exported
+func SayHay(name string) string {
+	return "Hai " + name
 }
