15dbe8361bad678cadfab7531636239758b654b9
andiagusprnm <aguspurnomoandi@gmail.com>
2022-03-02 11:58:03 +0700

    Test Tag Version

--- sayhello.go
+++ sayhello.go
@@ -1,13 +1,18 @@
 package sayhello
 
-import "strconv"
+import (
+	"fmt"
+	"strconv"
+)
 
-// SayHello is exported
 func SayHello(name string, year int) string {
 	return "Hello my name " + name + " age " + strconv.Itoa(year)
 }
 
-// SayHay is exported
 func SayHay(name string) string {
 	return "Hai " + name
+}
+
+func HelloGo() {
+	fmt.Println("Hello Go")
 }
